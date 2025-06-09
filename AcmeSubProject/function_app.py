f66e0f717bcf9c8d665d3d08ed075dffe5bbdaf5
Jhedie <jeddiahawuku12@gmail.com>
2025-06-10 00:48:19 +0100

    Enhance function app to store subtitles in Azure Table Storage and update deployment instructions

--- AcmeSubProject/function_app.py
+++ AcmeSubProject/function_app.py
@@ -4,6 +4,9 @@ import time
 import json
 import logging
 
+import uuid
+
+
 app = func.FunctionApp()
 
 
@@ -12,22 +15,34 @@ def translate(subtitle):
     # In a real-world scenario, this would call an external translation service
     return subtitle[::-1]  # Just reversing the string for demonstration purposes
 
+@app.table_output(
+    arg_name="table",
+    connection="AzureWebJobsStorage",
+    table_name="AcmeTranslations",
+    partition_key="table"
+)
 @app.route(route="AddSubtitle", auth_level=func.AuthLevel.ANONYMOUS)
-def AddSubtitle(req: func.HttpRequest) -> func.HttpResponse:
+def AddSubtitle(req: func.HttpRequest, table: func.Out[str]) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     start = time.time()
 
     req_body = req.get_json()
     subtitle = req_body.get('subtitle')
-    
-    translated = translate(subtitle)
+    row_key = str(uuid.uuid4())
+    #create the table entity
+    entity = {
+        "PartitionKey": "table",
+        "RowKey": row_key,
+        "Subtitle": subtitle
+    }
+    # Add the entity to the table output
+    table.set(json.dumps(entity))
 
-    time.sleep(5)  # Simulating 5 seconds of cpu-intensive processing
     end = time.time()
 
-    processing_time = end - start 
+    processing_time = end - start
     return func.HttpResponse(
-            f"Processing took {str(processing_time)} seconds. Translation is: {translated}",
+            f"Translation is: {subtitle} and it took {processing_time} seconds to process.",
             status_code=200
     )
